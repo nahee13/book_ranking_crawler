1f6bbf38d11f03814d8c9264aadbe614f4397c55
best33gh <83940722+best33gh@users.noreply.github.com>
2021-05-12 04:59:03 +0900

    bookbook_crawler.py

--- book_crawler.py
+++ book_crawler.py
@@ -3,6 +3,7 @@ import matplotlib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+
 html=urlopen("http://www.yes24.com/24/category/bestseller?CategoryNumber=001&sumgb=06")
 soup=BeautifulSoup(html, "lxml")
 #print(soup)
@@ -52,3 +53,50 @@ for rank,info in book_ranking.items():
 #print(kind_list)
 #print(book_title_list)
 #print(price_list)
+
+
+import matplotlib.font_manager as fm
+font_fname = 'C:/Windows/Fonts/NanumGothic.ttf'
+font_family = fm.FontProperties(fname=font_fname).get_name()
+plt.rcParams["font.family"] = font_family
+plt.rcParams['font.size'] = 15.
+plt.rcParams['xtick.labelsize'] = 6.
+plt.rcParams['ytick.labelsize'] = 6.
+plt.rcParams['axes.labelsize'] = 10.
+
+money=[]
+
+for i in soup.find(id='category_layout').find_all('td', {'class': 'goodsTxtInfo'}):
+    for price in i.find_all('span', {'class': 'priceB'}):
+        money.append(price.get_text())
+
+money=list(money)
+
+r_money=[]
+
+for i in money:
+    i=list(i)
+    if "," in i:
+        i.remove(",")
+    if "원" in i:
+        i.remove("원")
+    i=str(i)
+    if len(i)==25:
+        i=i[2]+i[7]+i[12]+i[17]+i[22]
+    else:
+        i = i[2] + i[7] + i[12] + i[17]
+    i=int(i)
+
+    r_money.append(i)
+
+r_money=list(r_money)
+x=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+title=book_title_list
+
+plt.bar(x,r_money)
+
+
+plt.bar(x,r_money)
+plt.suptitle('yes24 국내도서 종합 베스트셀러 20위까지의 가격')
+plt.xticks(x,title,rotation='vertical')
+plt.show()
